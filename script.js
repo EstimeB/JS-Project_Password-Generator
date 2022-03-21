b4b67f7b36d3a9edc6117348a9d8c9159d93c677
Brithnie Estime <estimebrithnie@yahoo.com>
2022-03-21 09:42:37 -0700

    Removed the created elements, renamed the arrays, updated the functions, made changes under event listener.

--- script.js
+++ script.js
@@ -1,39 +1,52 @@
+
+// Arrays
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t','u', 'v', 'w', 'x', 'y', 'z'];
+let upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'p', 'Q', 'R', 'S','T','U', 'V', 'W', 'X', 'Y', 'Z'];
+let symbols = ['!', '@', '#', '$', '%', '^', '&', '*'];
+let charLength = 8;
+let arrChoice = [];
+
 // Assignment Code
-let numbersEl = document.createElement("nUmbers");
-let lowerCaseEl = document.createElement("lowerCase");
-let upperCaseEl = document.createElement("upperCase");
-let symbolsEl = document.createElement("sYmbols");
-let lenghtEl = document.createElement("charLength");
-
-let nUmbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t','u', 'v', 'w', 'x', 'y', 'z'];
-let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'p', 'Q', 'R', 'S','T','U', 'V', 'W', 'X', 'Y', 'Z'];
-let sYmbols = ['!', '@', '#', '$', '%', '^', '&', '*'];
-
-let randomNumbers = nUmbers.concat(lowerCase, upperCase, sYmbols);
-
-function generatePassword() {
-  let password = ""
-  let charLength = prompt("How many characters would you like in your password?")
-  // Validation
-  while ( parseInt(charLength) < "8" || parseInt(charLength) > 128 ) {
-    charLength = prompt("Must be between 8 and 128 characters")
-  }
-} 
-
 let generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword();
+  let accPrompts = prompts();
   let passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  if (accPrompts) {
+    let genPassword = generatePassword();
+    passwordText.value = genPassword;
+  }
+  else {
+    passwordTest.value = "";
+  }
 
 }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); 
 
-if (charLength >= 8 && charLength <= 128) {
-  passwordText += prompt("randomNumbers")();
+// Generate password function
+function generatePassword() {
+  let password = "";
+  for (let i = 0; i < charLength; i++) {
+      let randomIndex = Math.floor(Math.random() * accPrompts.length);
+      password = password + accPrompts[randomIndex];
+  }
+  return password;
 }
 
+// Generate functions
+function prompts() {
+  accPrompts = [];
+  charLength = parseInt(prompt("How many characters would you like in your password?"));
+  if (isNaN(charLength) || charLength < 8 || charLength > 128 ) {
+      alert("Must be between 8 and 128 characters.");
+      return;
+  }
+  if (confirm) {
+    arrChoice = arrChoice.concat(upper,lower, numbers, symbols);
+  }
+  return true;
+}
